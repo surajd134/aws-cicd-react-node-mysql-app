0579335860684e59c0f278c0e0244991d56f5b88
ReyazShaik <45187103+ReyazShaik@users.noreply.github.com>
2025-01-12 14:35:15 +0530

    Update cleanup.sh

--- backend/scripts/cleanup.sh
+++ backend/scripts/cleanup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 # Ensure the backend directory exists
 echo "Ensuring the backend directory exists..."
 sudo mkdir -p /srv/react-node-mysql-app/backend
 echo "Backend directory is ready at /srv/react-node-mysql-app/backend"
-
 # Clear all files and hidden files in the backend directory
 echo "Clearing all files and hidden files in the backend directory..."
 sudo rm -rf /srv/react-node-mysql-app/backend/{*,.*} 2>/dev/null
